7963880bb3417aac171aac95302607a7d59d9b37
Frantz <fpaul787@gmail.com>
2021-01-02 00:52:00 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='players',
+      version='1.0',
+      description='Sports Players',
+      packages=['sports_players'],
+      install_requires=[
+          'pandas',
+      ],
+      author='Frantz Paul',
+      author_email= "fpaul787@gmail.com",
+      zip_safe=False)
