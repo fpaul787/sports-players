1349d92617d27f6e69a59cf50f8a2b35df0163f4
Frantz Paul <fpaul787@gmail.com>
2021-01-03 14:38:56 -0500

    removing README from sport-players directory and changing setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="sports-players",
-    version="1.0",
+    version="1.2",
     description="Sports Players Objects",
     packages=["sports_players"],
     install_requires=[
@@ -11,4 +18,6 @@ setup(
     author="Frantz Paul",
     author_email="fpaul787@gmail.com",
     zip_safe=False,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
 )
